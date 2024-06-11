8d88f7520c62edc3b63129f44a0f4fe9dcbeceb8
Székely Ádám <szekely.adam23@gmail.com>
2024-06-11 20:40:00 +0200

    Date format corrected

--- Frontend/src/util/formatDate.tsx
+++ Frontend/src/util/formatDate.tsx
@@ -1,15 +1,9 @@
+import { parseISO, format, addHours } from 'date-fns';
+
 const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    return date.toLocaleString('hu-HU', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-        timeZone: 'UTC',
-        timeZoneName: 'short' // Optional: to include time zone information
-    }).replace(',', '');
+    const date = parseISO(dateString);
+    const dateUTC2 = addHours(date, 2);
+    return format( dateUTC2, 'yyyy-MM-dd HH:mm:ss');
 };
 
 export default formatDate;
